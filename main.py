9bed02c8fa658e7b40340df90033769e1f5bb1d3
molrobot <john19990708@gmail.com>
2020-01-14 14:33:13 +0800

    update

--- main.py
+++ main.py
@@ -69,10 +69,7 @@ def dashboard_ec2():
     response = client.describe_instances()
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
-            # if instance['Tags']['Name'] == session.get('username'):
-                print(instance['Tags'])
-                print(instance['Tags'][0]['Key'])
-                print(instance['Tags'][0]['Value'])
+            if instance['Tags'][0]['Key'] == 'Name' and instance['Tags'][0]['Value'] == session.get('username'):
                 instances.append("Instance: " + instance['InstanceId'])
                 for securityGroup in instance['SecurityGroups']:
                     instances.append("SecurityGroup ID: {}, Name: {}".format(securityGroup['GroupId'], securityGroup['GroupName']))
